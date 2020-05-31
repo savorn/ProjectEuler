23ade0e8cfa5cb9ca4b9e19bd756f1e8da8f59e9
Savorn Meuk <savornmeuk@gmail.com>
2020-05-31 18:30:27 -0400

    Problem 6 solved

---
+++ Problem_1_to_50/Problem6.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+/*
+The sum of the squares of the first ten natural numbers is,
+1^2 + 2*2 + ... + 10^2 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)^2 = 55^2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 
+3025 - 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+int main() {
+    int sumSquares = 0;
+    int addNum = 0;
+
+    for (int i = 0; i <= 100; i++) {
+        sumSquares = sumSquares + (i * i);
+        addNum = addNum + i;
+    }
+    cout << (addNum * addNum) - sumSquares << "\n";
+    return 0;
+}
